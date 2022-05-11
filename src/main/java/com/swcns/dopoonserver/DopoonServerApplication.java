fac355ee4ce8d3ec8198675f66bee43eaf6505b0
VINTO1819 <develretr0@gmail.com>
2022-05-11 17:28:10 +0900

    Init project

---
+++ src/main/java/com/swcns/dopoonserver/DopoonServerApplication.java
@@ -0,0 +1,13 @@
+package com.swcns.dopoonserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DopoonServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DopoonServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/swcns/dopoonserver/DopoonServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swcns.dopoonserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DopoonServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
